8796a37ca5fe243c5182a0182c08e1d3957af5cc
Davi <davi@antonaji.cmo.br>
2022-09-03 17:19:23 -0300

    First commit

---
+++ ValidadorIP/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace ValidadorIP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lb_mensagem = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_token = new System.Windows.Forms.TextBox();
+            this.tb_chat_id = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnDesconectar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lb_mensagem
+            // 
+            this.lb_mensagem.AutoSize = true;
+            this.lb_mensagem.Location = new System.Drawing.Point(22, 9);
+            this.lb_mensagem.Name = "lb_mensagem";
+            this.lb_mensagem.Size = new System.Drawing.Size(71, 13);
+            this.lb_mensagem.TabIndex = 0;
+            this.lb_mensagem.Text = "Carregando...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bot Token:";
+            // 
+            // tb_token
+            // 
+            this.tb_token.Location = new System.Drawing.Point(25, 25);
+            this.tb_token.Name = "tb_token";
+            this.tb_token.Size = new System.Drawing.Size(284, 20);
+            this.tb_token.TabIndex = 2;
+            // 
+            // tb_chat_id
+            // 
+            this.tb_chat_id.Location = new System.Drawing.Point(25, 79);
+            this.tb_chat_id.Name = "tb_chat_id";
+            this.tb_chat_id.Size = new System.Drawing.Size(284, 20);
+            this.tb_chat_id.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Chat ID:";
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(25, 115);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(284, 34);
+            this.btnSalvar.TabIndex = 5;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnDesconectar
+            // 
+            this.btnDesconectar.Location = new System.Drawing.Point(25, 30);
+            this.btnDesconectar.Name = "btnDesconectar";
+            this.btnDesconectar.Size = new System.Drawing.Size(284, 34);
+            this.btnDesconectar.TabIndex = 6;
+            this.btnDesconectar.Text = "Desconectar";
+            this.btnDesconectar.UseVisualStyleBackColor = true;
+            this.btnDesconectar.Click += new System.EventHandler(this.btnDesconectar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 170);
+            this.Controls.Add(this.btnDesconectar);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.tb_chat_id);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_token);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lb_mensagem);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Monitoramento de IP - Telegram";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_mensagem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_token;
+        private System.Windows.Forms.TextBox tb_chat_id;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnDesconectar;
+    }
+}
+
---
+++ ValidadorIP/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.Json;
+using Newtonsoft.Json;
+using Telegram.Bot;
+
+namespace ValidadorIP
+{
+
+
+  
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            VerificaTelegram();
+            Thread t = new Thread(unused => verificarIP());
+            t.IsBackground = true;
+            t.Start();
+        }
+
+        public void verificarIP()
+        {
+            while (true)
+            {
+               
+
+                try
+                {
+                    string content = string.Empty;
+                    string url = @"https://api.ipify.org?format=json";
+
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                    request.AutomaticDecompression = DecompressionMethods.GZip;
+
+                    using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                    using (Stream stream = response.GetResponseStream())
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        content = reader.ReadToEnd();
+                    }
+
+
+                    var ip_body = JsonConvert.DeserializeObject<Dictionary<string, string>>(content);
+                    string ip = ip_body["ip"];
+
+                    string current_ip = "0.0.0.0";
+                    try
+                    {
+                        current_ip = File.ReadAllText("current_ip.txt");
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                    current_ip = current_ip.Replace("\n", "").Replace("\r", "");
+                    ip = ip.Replace("\n", "").Replace("\r", "");
+                    if (ip != current_ip)
+                    {
+                        string text = File.ReadAllText("telegram.txt");
+                        string[] c = text.Split('|');
+                        string chat_id = c[0].Replace("\n", "").Replace("\r", "");
+                        string token = c[1].Replace("\n", "").Replace("\r", "");
+                        Console.WriteLine("chat_id: " + chat_id);
+                        Console.WriteLine("token: " + token);
+                        TelegramBotClient Client = new TelegramBotClient(token);
+                        string message = "@-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-@\n" +
+                            "\n" +
+                            "IP ANTIGO:" + current_ip + "\n" +
+                            "IP NOVO:" + ip + "\n" +
+                            "\n" +
+                            "@-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-@";
+                        Client.SendTextMessageAsync(chat_id, message);
+                      
+                       
+                        string[] lines =
+                        {
+                            ip
+                        };
+
+                        File.WriteAllLines("current_ip.txt", lines);
+
+                    }
+
+
+
+                }
+                catch(Exception)
+                {
+                        
+                }
+
+
+                Thread.Sleep(60000);
+            }
+            
+            
+        }
+
+
+        private bool VerificaTelegram()
+        {
+            try
+            {
+
+
+                string text = File.ReadAllText("telegram.txt");
+
+                lb_mensagem.Text = "Conectado ao Telegram...";
+
+                lb_mensagem.Visible = true;
+                btnDesconectar.Visible = true;
+
+                btnSalvar.Visible = false;
+
+                btnSalvar.Visible = false;
+                label1.Visible = false;
+                label2.Visible = false;
+                tb_chat_id.Visible = false;
+                tb_token.Visible = false;
+                return true;
+
+            }
+            catch (Exception)
+            {
+                btnDesconectar.Visible = false;
+                lb_mensagem.Visible = false;
+
+                btnSalvar.Visible = true;
+                label1.Visible = true;
+                label2.Visible = true;
+                tb_chat_id.Visible = true;
+                tb_token.Visible = true;
+                return false;
+            }
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            string chat_id = tb_chat_id.Text;
+            string token = tb_token.Text;
+            if(chat_id != "" && token != "")
+            {
+
+                string[] lines =
+                {
+                    chat_id+"|"+token
+                };
+
+                File.WriteAllLines("telegram.txt", lines);
+                VerificaTelegram();
+            }
+        }
+
+        private void btnDesconectar_Click(object sender, EventArgs e)
+        {
+            File.Delete("telegram.txt");
+            VerificaTelegram();
+        }
+    }
+}
